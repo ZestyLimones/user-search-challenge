8de498d6f2044142d53b67305b0846c528e0c15f
Micheal Limones <micheal@zestylimones.com>
2021-11-12 09:46:30 -0700

    updated code to get posts to render to page.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const usersEl = $('#users');
-const postsEl = $('#user-posts')
+const postsEl = $('#user-posts');
 
 const apiUrl = 'https://jsonplaceholder.typicode.com/';
 
@@ -29,35 +29,36 @@ const getUsers = (e) => {
   });
 };
 
-const displayPosts(postData) {
-    postData.forEach((element) => {
-        let postTitle = element.title;
-        let postBody = element.body;
+const displayPosts = (postData) => {
+  postData.forEach((element) => {
+    let postTitle = element.title;
+    let postBody = element.body;
 
-        let addPostContainer = $('<div>');
-        addPostContainer.attr('class', 'container post-container');
-        let addTitle = $('<h3>');
-        addTitle.text(postTitle);
-        let addBody = $('<p>');
-        addBody.text(postBody);
+    let addPostContainer = $('<div>');
+    addPostContainer.attr('class', 'container post-container');
+    let addTitle = $('<h3>');
+    addTitle.text(postTitle);
+    let addBody = $('<p>');
+    addBody.text(postBody);
 
-        addTitle.appendTo(addPostContainer);
-        addBody.appendTo(addPostContainer);
-        addPostContainer.appendTo(postsEl)
-
-    })
-}
+    addTitle.appendTo(addPostContainer);
+    addBody.appendTo(addPostContainer);
+    addPostContainer.appendTo(postsEl);
+  });
+};
 
 const getPosts = (user) => {
-  let posts = `${apiUrl}posts/${userId}`;
+  let posts = `${apiUrl}users/1/posts`;
 
   fetch(posts).then((response) => {
     if (response.ok) {
       response.json().then((data) => {
-        diplayPosts(data);
+        displayPosts(data);
       });
     }
   });
 };
+
+getPosts();
 
 getUsers();
