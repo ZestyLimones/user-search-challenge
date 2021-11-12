e766ee432b5380c0f0b336267096f0144a1f60b0
Micheal Limones <micheal@zestylimones.com>
2021-11-12 09:06:18 -0700

    updated html to be a table. created display user function.

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-const usersEl = document.querySelector('#users');
+const usersEl = $('#users');
 
 const apiUrl = 'https://jsonplaceholder.typicode.com/';
 
-const getUser = (e) => {
+displayUsers(users) {
+    users.forEach(element => {
+        let name = element.name;
+
+        let addUserContainer = $('<tr>');
+        addUserContainer.attr('class', 'user-container');
+        let addUserName = $('<td>')
+
+        addUserName.appendTo(addUser);
+    });
+    
+
+
+}
+
+const getUsers = (e) => {
   const usersApi = `${apiUrl}users/`;
 
   fetch(usersApi).then((response) => {
     if (response.ok) {
-      response.json().then((data) => {});
+      response.json().then((data) => {
+        displayUsers(data);
+      });
     }
   });
 };
+
